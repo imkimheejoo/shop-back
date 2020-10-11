9fddb0431636dd54d04c80db5a6d8bed0cb35a9f
imkimheejoo <jujudamdadi@naver.com>
2020-10-11 16:46:06 +0900

    add: 상품조회시 상품옵션의 추가금액 변수 추가

--- src/main/java/com/shop/demo/dto/query/OptionInfo.java
+++ src/main/java/com/shop/demo/dto/query/OptionInfo.java
@@ -10,9 +10,11 @@ public class OptionInfo {
 
     private Long id;
     private String name;
+    private Long additionalPrice;
 
     public OptionInfo(ProductOption productOption) {
         this.id = productOption.getId();
         this.name = productOption.getOptionName();
+        this.additionalPrice = productOption.getOptionPrice().getMoney();
     }
 }
