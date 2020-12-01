f3fa4ec38be883c18bfe84640c662577a5119b92
imkimheejoo <jujudamdadi@naver.com>
2020-12-01 20:16:27 +0900

    fix: 페이징 에러 수정

--- src/main/java/com/shop/demo/products/repository/ProductRepository.java
+++ src/main/java/com/shop/demo/products/repository/ProductRepository.java
@@ -16,10 +16,6 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "select new com.shop.demo.dto.query.ProductInfoDto(p.id, p.title, p.price.money, p.imageUrl) " +
-            "from Product p")
-    Page<ProductInfoDto> findProductsInfo(Pageable pageable);
-
     Page<Product> findByCategory(@Param("category") Category category, Pageable pageable);
 
     @Query(value = "select p " +
@@ -31,6 +27,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @EntityGraph(attributePaths = {"options"})
     Optional<Product> findById(Long productId);
 
+    @Query(value = "select new com.shop.demo.dto.query.ProductInfoDto(p.id, p.title, p.price.money, p.imageUrl) " +
+            "from Product p")
+    Page<ProductInfoDto> findProductsInfo(Pageable pageable);
     //distinct 는 한 줄이 완전히 똑같을 때 만 날라감 , id만 같을 경우에는 안됨 근데 JPA는 해줌(어플리케이션 단에서!, 디비단 아님!)
     @Query("select p from Product p")
     Page<Product> findProductsInfo2(Pageable pageable);
